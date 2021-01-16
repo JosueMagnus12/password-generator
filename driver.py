07c784c63fcb4dd103289ca20564c82942bffdbc
Josue Granados <josuegranados.1219@gmail.com>
2021-01-15 19:22:40 -0600

    added a promt, main method and if _name_ == _main_:

--- driver.py
+++ driver.py
@@ -3,30 +3,47 @@ import binary_search
 import password_generator
 import time
 
-my_password = password_generator.generate_password(15)
-passwords = [0] * 1_000_000
+def main():
+    #prompt
+    n_char = int(input("How many characters in the passwords? (a strong password contains at least 10 characters): "))
+    n_pass = int(input("How many passwords for the test? (big numbers are recommended): "))
 
-start = time.time()
-for i in range(1_000_000):
-    passwords[i] = password_generator.generate_password(15)
-end = time.time()
+    #creating the first password and the list
+    my_password = password_generator.generate_password(n_char)
+    passwords = [0] * n_pass
 
-start2 = time.time()
-merge_sort.merge_sort(passwords,0,999_999)
-end2 = time.time()
+    #filling the list up
+    start = time.time()
+    for i in range(n_pass):
+        passwords[i] = password_generator.generate_password(n_char)
+    end = time.time()
 
-start3 = time.time()
-result = binary_search.binary_search(passwords,0,999_999,my_password)
-end3 = time.time()
+    #sorting the list
+    start2 = time.time()
+    merge_sort.merge_sort(passwords,0,n_pass - 1)
+    end2 = time.time()
 
-print(passwords)
-print("Your password is: " + my_password)
+    #searching the password
+    start3 = time.time()
+    result = binary_search.binary_search(passwords,0,n_pass - 1,my_password)
+    end3 = time.time()
 
-if result:
-    print("Insecure generator")
-else:
-    print("Secure generator")
+    #results
+    print(passwords)
+    print("Your password is: " + my_password)
 
-print("Filling time: {}".format(end - start))
-print("Sorting time: {}".format(end2 - start2))
-print("Searching time: {}".format(end3 - start3))
+    if result:
+        print("Insecure generator")
+    else:
+        print("Secure generator")
+
+    total_time_filling = round(end - start,5)
+    total_time_sorting = round(end2 - start2,5)
+    total_time_searching = round(end3 - start3,5)
+
+    print("Filling time: {}s".format(total_time_filling))
+    print("Sorting time: {}s".format(total_time_sorting))
+    print("Searching time: {}s".format(total_time_searching))
+
+if __name__ == "__main__":
+    main()
