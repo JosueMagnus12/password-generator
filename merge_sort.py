1b32685d0a7f69ed692dddda180a3cfef0079421
Josue Granados <josuegranados.1219@gmail.com>
2021-01-15 19:36:01 -0600

    commenting the merge_sort file

--- merge_sort.py
+++ merge_sort.py
@@ -1,3 +1,6 @@
+#creates 2 arrays and copies the respective elements from
+#the real one, then performs the merging process comparing
+#elements in the sub-arrays and setting the values in the real array.
 def merge(array, index_first, middle, index_last):
     #creating virtual arrays
     virtual_size1 = middle - index_first + 1
@@ -9,7 +12,7 @@ def merge(array, index_first, middle, index_last):
         virtual_array1[i] = array[index_first + i]
     for j in range(virtual_size2):
         virtual_array2[j] = array[middle + 1 + j]
-    #comparing and setting
+    #comparing and changing values
     virtual_index1 = 0
     virtual_index2 = 0
     real_index = index_first
@@ -21,7 +24,7 @@ def merge(array, index_first, middle, index_last):
             array[real_index] = virtual_array2[virtual_index2]
             virtual_index2 +=1
         real_index += 1
-    #if there are remain elements
+    #if there are remaining elements
     while virtual_index1 < virtual_size1:
         array[real_index] = virtual_array1[virtual_index1]
         virtual_index1 += 1
@@ -32,7 +35,10 @@ def merge(array, index_first, middle, index_last):
         real_index += 1
 
 
-
+#this method only works with indices and give the merge method them
+#to actually change values in the real array
+#this with the purpose to avoid overcrowding memory with sub-arrays in stand-by mode
+#due to the recursive calls.
 def merge_sort(array, index_first, index_last):
     if index_first < index_last:
         middle = (index_first + index_last) // 2
